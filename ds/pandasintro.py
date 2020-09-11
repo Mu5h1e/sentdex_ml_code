3e6c8568178c3459c51ce5671020a5481b8cf2c8
Mu5h1e <mushiesidd@gmail.com>
2020-09-11 19:14:32 +0530

    Back at it

--- ds/pandasintro.py
+++ ds/pandasintro.py
@@ -3,3 +3,14 @@ import matplotlib as plt
 import matplotlib.style as style
 style.use('ggplot')
 # pandas is fast thats the scene 
+
+web_stats = {
+            'Day':[1,2,3,4,5,6],
+            'Visitors':[43, 53, 34, 45, 64, 34],
+            'Bounce_rate':[65,72,62,64,54,66]}
+
+df = pd.DataFrame(web_stats)
+print(df)
+print(df.head())
+print(df.tail())
+print(df.tail(2))
