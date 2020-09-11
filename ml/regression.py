a107a4e139a104d9e383b669f86cb986a289f980
Mu5h1e <mushiesidd@gmail.com>
2020-09-12 00:19:28 +0530

    features and labels

--- ml/regression.py
+++ ml/regression.py
@@ -1,11 +1,20 @@
-import quandl 
+import quandl, math
+import numpy as np
 import pandas as pd
-import sklearn 
-import matplotlib as plt
+# from sklearn import preprocessing, cross_validation, svm
+# from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 data_frame = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 data_frame['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Low']
 data_frame['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open']
 data_frame = data_frame[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+
+forecast_out = int(math.ceil(0.01*len(df)))
+
+data_frame['label'] = data_frame[forecast_col].shift(-forecast_out)
+data_frame.dropna(inplace=True)
+
 print(data_frame)
