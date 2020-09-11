10a8f34ef07c526b759e9704393e703e43963dc8
Mu5h1e <mushiesidd@gmail.com>
2020-09-12 00:27:25 +0530

    Depricated

--- ml/regression.py
+++ ml/regression.py
@@ -1,8 +1,10 @@
 import quandl, math
 import numpy as np
 import pandas as pd
-# from sklearn import preprocessing, cross_validation, svm
-# from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
+
+# Cross_validation has been depricated
 
 df = quandl.get('WIKI/GOOGL')
 data_frame = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
