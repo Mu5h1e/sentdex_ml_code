d6c132c79fc4771ce4b550fa6ae15f52f147744a
Mu5h1e <mushiesidd@gmail.com>
2020-09-11 23:04:40 +0530

    back at it

--- ml/regression.py
+++ ml/regression.py
@@ -1,7 +1,11 @@
 import quandl 
 import pandas as pd
 import sklearn 
+import matplotlib as plt
 
 df = quandl.get('WIKI/GOOGL')
-df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
-print(df)
+data_frame = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+data_frame['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Low']
+data_frame['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open']
+data_frame = data_frame[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+print(data_frame)
