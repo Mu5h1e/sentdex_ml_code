5f6de19b1434d4af0e2629b7c23bf494fd9d36a1
Mustafa Siddiqui <mustafasiddiqui@pop-os.localdomain>
2020-10-24 02:15:59 +0530

    sentdex version of plot

--- ds/Lec_2b_sentdex_version.py
+++ ds/Lec_2b_sentdex_version.py
@@ -2,11 +2,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('./datasets/avocado.csv')
-df['Date'] = pd.to_datetime(pd['Date'])
+df['Date'] = pd.to_datetime(df['Date'])
 df.set_index('Date', inplace=True)
 df.sort_index(inplace=True)
 
 Graph_DataFrame = pd.DataFrame()
 
-for _ in set(df['regions']):
+for _ in set(df['region'][:6]):
     print(_)
+    region_df = df.copy()[df['region'] == _]
+    region_df[f'{_}_RollingAveragePrice'] = region_df['AveragePrice'].rolling(25).mean()
+
+    if Graph_DataFrame.empty:
+        Graph_DataFrame = region_df[[f'{_}_RollingAveragePrice']]
+    
+    else:
+        Graph_DataFrame = Graph_DataFrame.join(region_df[f'{_}_RollingAveragePrice'])
+
+Graph_DataFrame.plot()
+plt.show()
+print(Graph_DataFrame)
