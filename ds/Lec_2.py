4990eaf8628feb925b6e2a882a7abd8ff27a1d69
Mustafa Siddiqui <mustafasiddiqui@pop-os.localdomain>
2020-10-24 00:29:19 +0530

    Lec2 basic plot

---
+++ ds/Lec_2.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('./datasets/avocado.csv')
+albany_df = df[ df['region'] == 'Albany']
+albany_df.set_index('Date', inplace=True)
+albany_df['AveragePrice'].plot()
+plt.show()
