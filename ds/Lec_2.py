4d5b2d809642484213322717b748659942a74361
Mustafa Siddiqui <mustafasiddiqui@pop-os.localdomain>
2020-10-24 00:43:50 +0530

    Lec-2 rolling avg, sorting index + deep copy?

--- ds/Lec_2.py
+++ ds/Lec_2.py
@@ -2,7 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('./datasets/avocado.csv')
-albany_df = df[ df['region'] == 'Albany']
+df['Date'] = pd.to_datetime(df['Date'])
+albany_df = df.copy()[ df['region'] == 'Albany']
 albany_df.set_index('Date', inplace=True)
-albany_df['AveragePrice'].plot()
+albany_df.sort_index(inplace=True)
+albany_df['RollingAveragePrice'] = albany_df['AveragePrice'].rolling(25).mean()
+albany_df.dropna(inplace=True)
+albany_df['RollingAveragePrice'].plot()
 plt.show()
