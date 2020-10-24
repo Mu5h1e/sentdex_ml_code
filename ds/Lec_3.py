39d5ca0c11324c32330f03aec606be9ea1cb33b8
Mustafa Siddiqui <mustafasiddiqui@pop-os.localdomain>
2020-10-24 21:36:48 +0530

    used groupby

--- ds/Lec_3.py
+++ ds/Lec_3.py
@@ -1,5 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('./datasets/Minimum_Wage_Data.csv', engine='python')
-print(df)
+df = pd.read_csv('./datasets/minwage.csv')
+
+minimum_wage_df = pd.DataFrame()
+
+for name, group in df.groupby('State'):
+    if minimum_wage_df.empty:
+        minimum_wage_df = group.set_index('Year')[['Low.2018']].rename(columns= {'Low.2018':name})
+    
+    else:
+        minimum_wage_df = minimum_wage_df.join(group.set_index('Year')[['Low.2018']].rename(columns={'Low.2018':name}))
+
+minimum_wage_df.plot()
+plt.show()
