073b274abfc37074733ad9aa523da645cfc5ff67
Mu5h1e <mushiesidd@gmail.com>
2020-10-26 04:57:49 +0530

    Lec3 fixed drop issue

--- practical_ml/regression_p3.py
+++ practical_ml/regression_p3.py
@@ -1,6 +1,8 @@
 import pandas as pd 
-import quandl
-import math 
+import quandl, math
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
 q_df = quandl.get('WIKI/GOOGL')
 df = pd.DataFrame()
@@ -10,5 +12,9 @@ df['PCT_CHANGE'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
 df = df.copy()[['Adj. Close', 'HL_PCT', 'PCT_CHANGE', 'Adj. Volume']]
 
 df['label'] = df['Adj. Close'].shift(-int(math.ceil(0.01*len(df))))
+df.dropna(inplace=True)
+
+x = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
 
 print(df.head())
