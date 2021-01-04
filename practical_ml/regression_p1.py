3e369585a79048d27ea07d6a9640f1d7781d91a2
Mu5h1e <mushiesidd@gmail.com>
2021-01-04 21:37:05 +0530

    restarting for smai

--- practical_ml/regression_p1.py
+++ practical_ml/regression_p1.py
@@ -1,12 +1,7 @@
 import quandl
-import pandas as pd 
+import pandas as pd
+
+quandl.ApiConfig.api_key = 'x6xRKDkfUUycoBs-Ksn-'
 
 df = quandl.get('WIKI/GOOGL')
-info_df = pd.DataFrame()
-info_df = df
-info_df = info_df.copy()[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
-info_df['HL_PCT'] = (info_df['Adj. High'] - info_df['Adj. Close']) / info_df['Adj. Close'] * 100
-info_df['PCT_CHANGE'] = (info_df['Adj. Close'] - info_df['Adj. Open']) / info_df['Adj. Open'] * 100
-
-info_df = info_df.copy()[['Adj. Close', 'HL_PCT', 'PCT_CHANGE', 'Adj. Volume']]
-print(info_df.head())
+print(df.head())
